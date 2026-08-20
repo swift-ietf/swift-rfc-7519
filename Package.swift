b42ad4b701913e194bb7985484450c7c45b3185b
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:14:56 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
@@ -32,10 +32,10 @@ extension Target.Dependency {
 let package = Package(
     name: "swift-rfc-7519",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
     ],
     products: [
         .library(name: "RFC 7519", targets: ["RFC 7519"]),
